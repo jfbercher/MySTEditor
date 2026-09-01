f24635851222de2c5f957050ccc3407b620e317e
Michael Gielda <mgielda@antmicro.com>
2026-09-01 17:27:10 +0200

    Folding of all sections in preview

--- src/MystEditor.jsx
+++ src/MystEditor.jsx
@@ -11,7 +11,6 @@ import { createMystState, MystState, predefinedButtons, defaultButtons } from ".
 import { batch, computed, signal, effect, useSignal, useSignalEffect } from "@preact/signals";
 import { MystContainer } from "./styles/MystStyles";
 import { syncCheckboxes } from "./markdown/markdownCheckboxes";
-import { toggleFoldOnClick } from "./text";
 import { TableOfContents } from "./components/TableOfContents";
 import ErrorModal from "./components/ErrorModal";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -250,7 +249,7 @@ function stopResize() {
                   onClick={(ev) => {
                     try {
                       if (options.onPreviewClick.value?.(ev)) return;
-                      if (toggleFoldOnClick(ev, preview.current)) return;
+                      if (text.toggleFoldOnClick(ev)) return;
 
                       /*const dropdownHeader = ev.target.closest(".admonition.dropdown > header");
                       if (dropdownHeader) {
